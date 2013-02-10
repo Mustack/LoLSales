f3be1948033de733ea71fb57a3280be48ee99b89
Sam Wilson <tecywiz121@hotmail.com>
2013-02-10 16:25:46 -0500

    Scraping works

--- champions/models.py
+++ champions/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Champion(models.Model):
+	subscribers = models.ManyToManyField(User, through = 'accounts.Subscription')
+
 	name = models.CharField(max_length=255, unique=True)
 	title = models.CharField(max_length=255)
-
-	subscribers = models.ManyToManyField(User, through = 'accounts.Subscription')
+	detail_url = models.URLField(max_length=1024)
+	icon_url = models.URLField(max_length=1024)
+	image_url = models.URLField(max_length=1024)
+	short_description = models.TextField()
+	description = models.TextField()
