e2affe25bd30eac3e1b86903f958149ff3029652
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-10 16:12:15 -0500

    Prepare TemplateView for Champion.

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from champions.models import Champion
-# Create your models here.
+
 
 class Subscription(models.Model):
-	user = models.ForeignKey(User)
-	group = models.ForeignKey(Champion)
+    user = models.ForeignKey(User)
+    group = models.ForeignKey(Champion)
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.http import HttpResponse
-from django.views.generic import View
-# Create your views here.
-class SubscriptionView(View):
+from django.views.generic import TemplateView
 
-	def get(self, request, *args, **kwargs):
-		return HttpResponse('Hello World!')
+
+class SubscriptionView(TemplateView):
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello World!')
