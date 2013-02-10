4945f80abb559af3ba10ee768006d6cc4173433f
Antoine Grondin <antoinegrondin@gmail.com>
2013-02-10 15:14:34 -0500

    Add models for subscription page.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from .. import Champion
 # Create your models here.
+
+class Subscription(models.Model):
+	user = models.ForeignKey(User)
+	group = models.ForeignKey(Champion)
--- champions/models.py
+++ champions/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Champion(models.Model):
+	name = models.CharField(max_lenght=255)
+	subscribers = models.ManyToManyField(User, through = 'Subscription')
