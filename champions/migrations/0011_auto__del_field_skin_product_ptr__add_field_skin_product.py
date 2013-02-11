c8f189397f3696fb91bd141ee824324aa0a05eec
Sam Wilson <tecywiz121@hotmail.com>
2013-02-11 11:20:53 -0500

    Use django-model-utils instead of the determiner stuff I was using

---
+++ champions/migrations/0011_auto__del_field_skin_product_ptr__add_field_skin_product.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Skin.product'
+        #db.alter_column('champions_skin', 'product_ptr_id',
+        #              self.gf('django.db.models.fields.related.OneToOneField')(default=0, to=orm['champions.Product'], unique=True, primary_key=True, db_column='product_ptr_id'),
+        #              keep_default=False)
+        pass
+
+
+    def backwards(self, orm):
+        #db.alter_column('champions_skin', 'product_ptr_id',
+        #              self.gf('django.db.models.fields.related.OneToOneField')(default=0, to=orm['champions.Product'], unique=True, primary_key=True, db_column='product_ptr_id'),
+        #              keep_default=False)
+        pass
+
+    models = {
+        'accounts.subscription': {
+            'Meta': {'object_name': 'Subscription'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'subscriptions'", 'to': "orm['champions.Product']"}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'subscriptions'", 'to': "orm['auth.User']"})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'champions.champion': {
+            'Meta': {'object_name': 'Champion', '_ormbases': ['champions.Product']},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'detail_url': ('django.db.models.fields.URLField', [], {'max_length': '1024'}),
+            'icon_url': ('django.db.models.fields.URLField', [], {'max_length': '1024'}),
+            'image_url': ('django.db.models.fields.URLField', [], {'max_length': '1024'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'product': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['champions.Product']", 'unique': 'True', 'primary_key': 'True'}),
+            'short_description': ('django.db.models.fields.TextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'champions.product': {
+            'Meta': {'object_name': 'Product'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subscribers': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'products'", 'symmetrical': 'False', 'through': "orm['accounts.Subscription']", 'to': "orm['auth.User']"})
+        },
+        'champions.sale': {
+            'Meta': {'object_name': 'Sale'},
+            'end': ('django.db.models.fields.DateField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'products': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'sales'", 'symmetrical': 'False', 'through': "orm['champions.SaleItem']", 'to': "orm['champions.Product']"}),
+            'start': ('django.db.models.fields.DateField', [], {})
+        },
+        'champions.saleitem': {
+            'Meta': {'object_name': 'SaleItem'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sale_items'", 'to': "orm['champions.Product']"}),
+            'sale': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sale_items'", 'to': "orm['champions.Sale']"})
+        },
+        'champions.skin': {
+            'Meta': {'object_name': 'Skin', '_ormbases': ['champions.Product']},
+            'champion': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['champions.Champion']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'product': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['champions.Product']", 'unique': 'True', 'primary_key': 'True', 'db_column': "'product_ptr_id'"})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['champions']
--- champions/models.py
+++ champions/models.py
@@ -3,35 +3,6 @@ from django.contrib.auth.models import User
 
 class Product(models.Model):
 	subscribers = models.ManyToManyField(User, through='accounts.Subscription', related_name='products')
-	determiner = models.IntegerField(default=0)
-
-	@property
-	def child(self):
-		if self.determiner == 1:
-			return self.champion
-		else:
-			return self._set_determiner()
-
-	def _set_determiner(self):
-		try:
-			child = self.champion
-			self.determiner = 1
-			return child
-		except Champion.DoesNotExist:
-			pass
-
-		try:
-			child = self.skin
-			self.determiner = 2
-			return child
-		except Skin.DoesNotExist:
-			pass
-
-		return None # No product attached? Probably an error...
-
-	def save(self, *args, **kwargs):
-		self._set_determiner()
-		return super(Product, self).save(*args, **kwargs)
 
 # Create your models here.
 class Champion(Product):
@@ -49,6 +20,7 @@ class Champion(Product):
 		return self.name
 
 class Skin(Product):
+	product = models.OneToOneField(Product, parent_link=True, db_column='product_ptr_id')
 	name = models.CharField(max_length=255)
 	champion = models.ForeignKey(Champion)
 
